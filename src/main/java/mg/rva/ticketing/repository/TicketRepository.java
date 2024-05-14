c9b78dcb1e36b8c5974d20e680c2dc7dec2e3471
DESKTOP-E4FVGN5\Vola <volaainaramamonjisoa@gmail.com>
2024-05-14 20:01:13 +0300

    Forgotten @Repository annotation

--- src/main/java/mg/rva/ticketing/repository/TicketRepository.java
+++ src/main/java/mg/rva/ticketing/repository/TicketRepository.java
@@ -3,7 +3,9 @@ package mg.rva.ticketing.repository;
 import mg.rva.ticketing.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
     @Query(value="SELECT t FROM Ticket t WHERE t.id=?1")
--- src/main/java/mg/rva/ticketing/repository/UserRepository.java
+++ src/main/java/mg/rva/ticketing/repository/UserRepository.java
@@ -4,9 +4,11 @@ import mg.rva.ticketing.model.Ticket;
 import mg.rva.ticketing.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(value="SELECT u FROM User u WHERE u.id=?1")
